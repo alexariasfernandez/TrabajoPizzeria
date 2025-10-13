ab3601b26ffa9e10d5ac82eb8d1e6c4a4b377532
alex.ariasfernandez <alexitoefepe@gmail.com>
2025-10-13 17:13:41 +0200

    DAOs

--- PizzeriaCosaNostra-master/src/main/java/dao/IngredienteDAO.java
+++ PizzeriaCosaNostra-master/src/main/java/dao/IngredienteDAO.java
@@ -1,4 +1,9 @@
 package dao;
 
+import model.Ingrediente;
+
+import java.util.List;
+
 public interface  IngredienteDAO {
+    public List<Ingrediente> getAll();
 }
--- PizzeriaCosaNostra-master/src/main/java/dao/PizzaDAO.java
+++ PizzeriaCosaNostra-master/src/main/java/dao/PizzaDAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import model.Pizza;
+import service.PizzaService;
+
+import java.util.List;
+
 public interface  PizzaDAO {
+    public List<Pizza> readFromJSON();
+    public List<Pizza> readFromXML();
+    public void añadir(Pizza pizza);
+    public void modificar(Pizza pizza);
 }
