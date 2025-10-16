74f0f64aef6dc55df3f0e4a587758b54e57b5df2
xoel.lagohermida <xellaago@gmail.com>
2025-10-16 16:38:53 +0200

    Properties

--- PizzeriaCosaNostra-master/src/main/java/service/LandingPageServiceMock.java
+++ PizzeriaCosaNostra-master/src/main/java/service/LandingPageServiceMock.java
@@ -12,5 +12,6 @@ public class LandingPageServiceMock implements LandingPageService {
         return new LandingPageContent("Este es el contenido Mock de la sección quienesSomos",
                 "Este es el contenido Mock de la sección amor por los productos",
                 "Estw es el contenid Mock de la sección experiencia ");
+
     }
 }
